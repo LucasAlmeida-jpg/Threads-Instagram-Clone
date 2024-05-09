e1fa27894e7c57f81fea521879b04ca9bdd2683f
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2024-05-09 12:32:28 -0300

    pinia included and user  exported

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -11,7 +11,13 @@
         <div id="BottomNav" class="fixed z-50 bottom-0 flex w-full h-[70px] border-t border-t-gray-700 bg-black">
             <div class="flex w-full max-w-[500px] mx-auto flex items-center justify-around">
                 <button @click="navigateTo('/')" class="w-full h-full">
-                <Icon name="material-symbols:home-outline" size="35" color="#ffffff" />
+                <Icon name="material-symbols:home-outline" size="32" color="#ffffff" />
+                </button>
+                <button @click="userStore.isMenuOverlay = tryUseNuxt" class="w-full h-full">
+                <Icon name="material-symbols:edit-square-outline-rounded" size="32" color="#ffffff" />
+                </button>
+                <button @click="userStore.isLogoutOverlay = tryUseNuxt" class="w-full h-full">
+                <Icon name="ph:sign-out" size="32" color="#ffffff" />
                 </button>
             </div>
         </div>
@@ -19,8 +25,8 @@
 </template>
 
 <script setup>
-import { navigateTo } from 'nuxt/app';
-
+// import {useUserStore} from '/stores/user'
+// const userStore = useUserStore();
 
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,36 @@
 <template>
   <MainLayout>
-    <h1>INDICEEEEEEEE</h1>
+    <div id="IndexPage" class="w-full overflow-auto">
+      <div class="mx-auto max-w-[500px] overflow-hidden">
+      <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+        <div class="text-white" v-if="isPosts" v-for="post in posts" :key="post">
+          {{ post }}
+        </div>
+      </div>
+      </div>
+    </div>
   </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from '../layouts/MainLayout.vue';
+import {useUserStore} from '../stores/user';
+const userStore = useUserStore();
+
+let posts = ref([]); 
+let isPosts = ref(true);
+let isLoading = ref(true);
+
+onBeforeMount(() => {
+  posts.value =  [
+    {
+      name: 'Lucas',
+      image: 'https://placehold.co/100',
+      text: 'Este é o título do Lucas', 
+      picture: 'https://placehod.co/500'
+    }
+  ]
+})
 </script>
 
 
